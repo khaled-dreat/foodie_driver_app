4429b2f75b904d7a269c99a81d32f6a197996335
khaled-dreat <dreatkhaled@gmail.com>
2024-05-25 15:47:14 +0300

    Delete tracking order feature files
    Deleted all files related to the tracking order feature, including data sources, models, repositories, entities, use cases, presentation cubit, state, pages, and widgets.

--- lib/foodie_driver_app/features/tracking_order/presentation/cubit/tracking_order_cubit.dart
+++
@@ -1,8 +0,0 @@
-import 'package:bloc/bloc.dart';
-import 'package:equatable/equatable.dart';
-
-part 'tracking_order_state.dart';
-
-class TrackingOrderCubit extends Cubit<TrackingOrderState> {
-  TrackingOrderCubit() : super(TrackingOrderInitial());
-}
