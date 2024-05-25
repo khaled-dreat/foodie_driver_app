f65ea04db92f62d6fbcf7c2156cd0a892e840110
khaled-dreat <dreatkhaled@gmail.com>
2024-05-25 21:40:11 +0300

    Refactor AppStart widget in foodie_driver_app

--- lib/foodie_driver_app/app_start.dart
+++ lib/foodie_driver_app/app_start.dart
@@ -5,12 +5,11 @@ import '../features/home/presentation/pages/home/home_view.dart';
 class AppStart extends StatelessWidget {
   const AppStart({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const HomeView(),
+      home: HomeView(),
     );
   }
 }
