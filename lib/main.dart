3dfbc56bf59d56e77c0c407d8a2e264f4441f835
khaled-dreat <dreatkhaled@gmail.com>
2024-06-02 21:16:08 +0300

    Add Bloc observer and setup service locator in main.dart file
    This commit adds a Bloc observer to the main function and sets up the service locator for the API service and map in the main.dart file. The SimpleBlocObserver is used to observe the Bloc events and the setupServiceLocatorApiService() and setupServiceLocatorMap() functions are called to set up the service locator.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:foodie_driver_app/utils/setup_service_locator/setup_service_locator.dart';
 
 import 'foodie_driver_app/app_start.dart';
+import 'utils/simple_bloc_observer/simple_bloc_observer.dart';
 
 void main() {
+  Bloc.observer = SimpleBlocObserver();
+  setupServiceLocatorApiService();
+  setupServiceLocatorMap();
   runApp(const AppStart());
 }
