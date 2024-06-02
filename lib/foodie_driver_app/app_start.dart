ddc78f2687af8bcdbb44254b19663f8fb5e415d1
khaled-dreat <dreatkhaled@gmail.com>
2024-06-02 21:46:30 +0300

    Refactor AppStart widget and add MapCubit to MultiBlocProvider
    The commit refactors the AppStart widget by adding a MultiBlocProvider and a list of providers that includes the MapCubit. The MapCubit is created using the FetchRoutesUseCase and the MapRepoEmpl. This change improves the organization and structure of the code.

--- lib/foodie_driver_app/app_start.dart
+++ lib/foodie_driver_app/app_start.dart
@@ -1,15 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:foodie_driver_app/features/home/data/repositories/map_repo_empl.dart';
+import 'package:foodie_driver_app/features/home/domain/usecases/fetch_routes_use_case.dart';
+import 'package:foodie_driver_app/features/home/presentation/cubit/map_cubit.dart';
+import 'package:foodie_driver_app/utils/setup_service_locator/setup_service_locator.dart';
 
 import '../features/home/presentation/pages/home/home_view.dart';
+import 'package:nested/nested.dart';
 
 class AppStart extends StatelessWidget {
   const AppStart({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomeView(),
+    return MultiBlocProvider(
+      providers: providers,
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: HomeView(),
+      ),
     );
   }
+}
+
+List<SingleChildWidget> get providers {
+  return [
+    BlocProvider(
+      create: (context) =>
+          MapCubit(FetchRoutesUseCase(mapRepo: getIt.get<MapRepoEmpl>())),
+    )
+  ];
 }
