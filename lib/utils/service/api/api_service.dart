bd4642d22037dbc75217bf9dfadfaa70964ab2c7
khaled-dreat <dreatkhaled@gmail.com>
2024-06-02 21:47:09 +0300

    Refactor ApiService to include new endpoints and headers
    This commit refactors the ApiService to include new endpoints for getting place details and posting routes. It also adds new headers for the routes endpoint. The changes include importing new models and the ApiKey class.

--- lib/utils/service/api/api_service.dart
+++ lib/utils/service/api/api_service.dart
@@ -2,19 +2,55 @@ import 'dart:developer';
 
 import 'package:dio/dio.dart';
 
+import '../../../features/home/data/models/location_info/location_info.dart';
+import '../../../features/home/data/models/routes_modifiers.dart';
+import 'api_key.dart';
+
 class ApiService {
   final Dio _dio;
 
-  final String baseUrl = 'https://maps.googleapis.com/maps/api/place';
+  final String placeBaseUrl = 'https://maps.googleapis.com/maps/api/place';
+  String routesBaseUrl =
+      'https://routes.googleapis.com/directions/v2:computeRoutes';
   final String apiKey = 'AIzaSyC87Tt3tfO6aYids0BZStXXbrdAy05jQCI';
 
   ApiService(this._dio);
 
-  Future<Map<String, dynamic>> get({required String endPoint}) async {
-    log(name: "Url", endPoint);
-    var response = await _dio
-        .get('  $baseUrl/autocomplete/json?key=$apiKey&input=تايجر لاند');
+  Future<Response> getPlaceDetails({required String placeId}) async {
+    return await _dio.get(
+        '$placeBaseUrl/details/json?key=${ApiKey.apiKeyRoutes}&place_id=$placeId');
+  }
 
-    return response.data;
+  Future<Response> postRoutes(
+      {required LocationInfoModel origin,
+      required LocationInfoModel destination,
+      RoutesModifiers? routesModifiers}) async {
+    Map<String, String> headers = {
+      'Content-Type': 'application/json',
+      'X-Goog-Api-Key': ApiKey.apiKeyRoutes,
+      'X-Goog-FieldMask':
+          'routes.duration,routes.distanceMeters,routes.polyline.encodedPolyline'
+    };
+    Map<String, dynamic> body = {
+      "origin": origin.toJson(),
+      "destination": destination.toJson(),
+      "travelMode": "DRIVE",
+      "routingPreference": "TRAFFIC_AWARE",
+      "computeAlternativeRoutes": false,
+      "routeModifiers": routesModifiers != null
+          ? routesModifiers.toJson()
+          : RoutesModifiers().toJson(),
+      "languageCode": "en-US",
+      "units": "IMPERIAL"
+    };
+
+    var response = await _dio.post(
+      routesBaseUrl,
+      options: Options(
+        headers: headers,
+      ),
+      data: body,
+    );
+    return response;
   }
 }
